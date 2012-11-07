9e868a98781c1d2e0903ccdaf38777176a019237
Thomas Fini Hansen <xen@xen.dk>
2012-11-07 08:44:15 +0100

    Adjusted faces for Magit.

--- init.el
+++ init.el
@@ -1053,6 +1053,10 @@ or a marker."
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
+ '(magit-diff-add ((t (:foreground "light green"))))
+ '(magit-diff-del ((t (:foreground "tomato"))))
+ '(magit-item-highlight ((t (:background "#111"))))
+ '(mode-line ((((class color) (min-colors 88)) (:background "grey75" :foreground "black" :box (:line-width -1 :color "grey50")))))
  '(region ((t (:background "#456"))))
  '(secondary-selection ((((class color) (min-colors 88) (background dark)) (:background "Blue3")))))
 (put 'narrow-to-region 'disabled nil)
