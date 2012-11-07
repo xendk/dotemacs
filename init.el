672d499047b1d035926319ae2fec2602433610fa
Thomas Fini Hansen <xen@xen.dk>
2012-11-07 08:43:58 +0100

    Disabled el4r.

--- init.el
+++ init.el
@@ -12,9 +12,9 @@
 
 ;; Beginning of the el4r block:
 ;; RCtool generated this block automatically. DO NOT MODIFY this block!
-(add-to-list 'load-path "/usr/share/emacs/site-lisp")
-(require 'el4r)
-(el4r-boot)
+;(add-to-list 'load-path "/usr/share/emacs/site-lisp")
+;(require 'el4r)
+;(el4r-boot)
 ;; End of the el4r block.
 ;; User-setting area is below this line.
 
