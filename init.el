084da9ddd37c28578e8690aaf45609b9fba7376b
Thomas Fini Hansen <xen@xen.dk>
2012-11-29 14:06:25 +0100

    Added duplicate line.

--- init.el
+++ init.el
@@ -225,6 +225,25 @@ arrow and marks next symbol."
 ; http://www.emacswiki.org/emacs/WholeLineOrRegion
 (require 'whole-line-or-region)
 
+; http://emacswiki.org/emacs/CopyingWholeLines
+;; duplicate current line
+(defun duplicate-current-line (&optional n)
+  "duplicate current line, make more than 1 copy given a numeric argument"
+  (interactive "p")
+  (save-excursion
+    (let ((nb (or n 1))
+    	  (current-line (thing-at-point 'line)))
+      ;; when on last line, insert a newline first
+      (when (or (= 1 (forward-line 1)) (eq (point) (point-max)))
+    	(insert "\n"))
+
+      ;; now insert as many time as requested
+      (while (> n 0)
+    	(insert current-line)
+    	(decf n)))))
+
+(global-set-key (kbd "C-S-d") 'duplicate-current-line)
+
 ; http://www.emacswiki.org/emacs/CopyAndPaste
 (setq x-select-enable-clipboard t)
 (setq interprogram-paste-function 'x-cut-buffer-or-selection-value)
