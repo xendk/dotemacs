579fbe0b1a4817540b0f08aca3fd609d24ad4e79
Thomas Fini Hansen <xen@xen.dk>
2013-02-18 10:12:47 +0100

    Less annoying magit-status behaviour.

--- init.el
+++ init.el
@@ -87,6 +87,22 @@
 ; Add shortcut to open magit status buffer.
 (global-set-key (kbd "C-c C-g") 'magit-status)
 
+; Magits window splitting is annoying. Make it full-window.
+(defadvice magit-status (around magit-fullscreen activate)
+  (window-configuration-to-register :magit-fullscreen)
+  ad-do-it
+  (delete-other-windows))
+
+; Restores the window configuration when quitting magit.
+(defun magit-quit-session ()
+  "Restores the previous window configuration and kills the magit buffer"
+  (interactive)
+  (kill-buffer)
+  (jump-to-register :magit-fullscreen))
+
+; Rebind q to our quitting function.
+(define-key magit-status-mode-map (kbd "q") 'magit-quit-session)
+
 ; Auto-Complete
 ; Setup.
 (require 'auto-complete-config)
