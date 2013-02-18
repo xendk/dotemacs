687949579f4e871afa0b47450f2cb843b62c6142
Thomas Fini Hansen <xen@xen.dk>
2013-02-18 20:14:10 +0100

    Don't disable auto-complete in strings and comments.

--- custom.el
+++ custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ac-disable-faces nil)
  '(ange-ftp-try-passive-mode t)
  '(auto-insert-mode t nil (autoinsert))
  '(browse-url-browser-function (quote browse-url-generic))
