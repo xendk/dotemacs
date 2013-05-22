30ad6f1d6493010bfb7e518c4310ed4c36b09096
Thomas Fini Hansen <xen@xen.dk>
2013-05-22 20:16:16 +0200

    Make flymake errors not mess up syntax highlighting.

--- custom.el
+++ custom.el
@@ -94,6 +94,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(flymake-errline ((t (:background "#4b0000"))))
  '(magit-diff-add ((t (:foreground "light green"))))
  '(magit-diff-del ((t (:foreground "tomato"))))
  '(magit-item-highlight ((t (:background "#111"))))
