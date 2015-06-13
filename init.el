a83a2547e956d13758c0b80271fb26727aab33a6
Thomas Fini Hansen <xen@xen.dk>
2015-06-13 23:39:05 +0200

    Use helm-company.

--- init.el
+++ init.el
@@ -214,7 +214,14 @@ See URL `https://github.com/nzakas/eslint'."
             (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
             (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
             (define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
+))
 
+(use-package helm-company
+  :config (eval-after-load 'company
+            '(progn
+               (define-key company-mode-map (kbd "C-:") 'helm-company)
+               (define-key company-active-map (kbd "C-:") 'helm-company)))
+  )
 
 (use-package helm-gtags
   :config (progn
