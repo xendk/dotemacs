8731878e2a90e7601a715711453bc9079f55d6b0
Thomas Fini Hansen <xen@xen.dk>
2016-03-01 08:52:35 +0100

    Use icon for magit-status-mode

--- init.el
+++ init.el
@@ -318,8 +318,8 @@ Format is PROJECT (CLIENT) \n TASK - NOTES"
 (use-package magit
   :init (setq magit-last-seen-setup-instructions "1.4.0")
   :config (add-hook 'magit-log-edit-mode-hook 'xen-magit-log-edit-mode-hook)
-
-  )
+  (delight 'magit-status-mode (propertize (concat " " [#xF1D3])
+                                  'face '(:family "FontAwesome")) :major))
 
 ;; Add git flow extension.
 (use-package magit-gitflow
