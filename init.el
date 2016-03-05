cfec6cef3ab4f97cdb60d842fc7125a4b5b8290c
Thomas Fini Hansen <xen@xen.dk>
2016-03-05 01:03:32 +0100

    Reactivate magit-gh-pulls mode

--- init.el
+++ init.el
@@ -328,9 +328,9 @@ Format is PROJECT (CLIENT) \n TASK - NOTES"
   (add-hook 'magit-mode-hook 'turn-on-magit-gitflow))
 
 ;; Add github pull request extension.
-;; (use-package magit-gh-pulls
-;;   :commands turn-on-magit-gh-pulls
-;;   :init (add-hook 'magit-mode-hook 'turn-on-magit-gh-pulls))
+(use-package magit-gh-pulls
+  :commands turn-on-magit-gh-pulls
+  :init (add-hook 'magit-mode-hook 'turn-on-magit-gh-pulls))
 
 (use-package markdown-mode
   :mode (("\\.\\(m\\(ark\\)?down\\)$" . markdown-mode)
