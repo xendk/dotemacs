9dd0458e10ff9f0d93f46b13e6ea43b284a84b76
Thomas Fini Hansen <xen@xen.dk>
2016-12-25 22:49:32 +0100

    Remove unused code

--- xen/xen.el
+++ xen/xen.el
@@ -339,23 +339,6 @@ Actually shrinks the region if the point is at the start of the region."
     )
   )
 
-;; Gotten from http://endlessparentheses.com/easily-create-github-prs-from-magit.html
-(defun xen/visit-pull-request-url ()
-  "Visit the current branch's PR on Github."
-  (interactive)
-  (browse-url
-   (format "https://github.com/%s/compare/%s"
-     (replace-regexp-in-string
-      "\\`.+github\\.com:\\(.+\\)\\.git\\'" "\\1"
-      (magit-get "remote"
-                 (magit-get-current-remote)
-                 "url"))
-     (magit-get-current-branch))))
-
-(eval-after-load 'magit
-  '(define-key magit-mode-map "V"
-     #'xen/visit-pull-request-url))
-
 (defun xen-xml-pretty ()
   "Run xmllint -pretty - on the region."
   (interactive)
