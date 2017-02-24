f90a457a589bf646d3ec8b653a30a1c8560126aa
Thomas Fini Hansen <xen@xen.dk>
2017-02-24 23:19:52 +0100

    Add melpa-stable

--- init.el
+++ init.el
@@ -53,6 +53,8 @@
                "http://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives
+             '("melpa-stable" . "http://melpa-stable.milkbox.net/packages/") t)
 (package-initialize)
 
 ;; Bootstrap `use-package'
