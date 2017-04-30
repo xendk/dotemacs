3548a44c870e76436bde5b05ada82e668b649ffd
Thomas Fini Hansen <xen@xen.dk>
2017-04-30 22:52:45 +0200

    Try out having avy dim background

--- custom.el
+++ custom.el
@@ -13,6 +13,7 @@
  '(auto-indent-blank-lines-on-move nil)
  '(auto-indent-next-pair nil)
  '(auto-insert-mode t nil (autoinsert))
+ '(avy-background t)
  '(avy-keys
    (quote
     (97 98 99 100 101 102 103 104 105 106 107 108 109 110 111 112 113 114 115 116 117 118 119 120 121 122)))
