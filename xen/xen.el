c79146cc679586a6770e92405591c3d84fac7755
Thomas Fini Hansen <xen@xen.dk>
2017-12-17 20:15:36 +0100

    Rewrite paired-delete to use sp-unwrap

--- xen/xen.el
+++ xen/xen.el
@@ -118,32 +118,31 @@ only work when ARG is 1 or the region is not active."
              smartparens-mode
              (not (use-region-p)))
         (-if-let (ok (sp-get-thing backwards-p))
-            (sp-get ok (progn
-                         ;; If either open or close is empty, bomb
-                         ;; out. This is the case for symbols, and
-                         ;; anyway it doesn't make sense.
-                         (if (not (or (equal :op "") (equal :cl "")))
-                             (save-excursion
-                               (if (= (- (point) (if backwards-p 1 0)) :beg)
-                                   (progn (goto-char :end)
-                                          (delete-char -1)
-                                          ;; This is odd, but without
-                                          ;; this, it would chomp 2
-                                          ;; chars when deleting
-                                          ;; forwards.
-                                          (goto-char :beg)
-                                          ))
-                               (if (= (+ (point) (if backwards-p 0 1)) :end)
-                                   (progn (goto-char :beg)
-                                          (delete-char 1))))))))))
+            (progn
+              (sp-get ok (progn
+                           ;; If either open or close is empty, bomb
+                           ;; out. This is the case for symbols, and
+                           ;; anyway it doesn't make sense.
+                           (if (not (or (equal :op "") (equal :cl "")))
+                               (save-excursion
+                                 (if (= (- (point) (if backwards-p 1 0)) :beg)
+                                     (if backwards-p (sp-backward-unwrap-sexp)
+                                       (sp-unwrap-sexp)))
+                                 (if (= (+ (point) (if backwards-p 0 1)) :end)
+                                     (if backwards-p (sp-backward-unwrap-sexp)
+                                       (sp-unwrap-sexp)))
+                                 ok)
+                             nil)))))))
 
-(defun xen-delete-char-advice (n &optional kill-flag)
-  "Advice for delete char.  Use N and ignore KILL-FLAG."
+(defun xen-delete-char-advice (orig-fun n &optional kill-flag)
+  "Advice for delete char.  ORIG-FUN is the overriden function. Use N and ignore KILL-FLAG."
   (if (not (boundp 'xen-delete-char-disabled))
       (let ((xen-delete-char-disabled t))
         (save-match-data (progn
-                           (xen-paired-delete (> 0 n) (abs n)))))))
-(advice-add 'delete-char :before #'xen-delete-char-advice)
+                           (if (not (xen-paired-delete (> 0 n) (abs n)))
+                               (apply orig-fun n kill-flag)))))
+    (apply orig-fun n kill-flag)))
+(advice-add 'delete-char :around #'xen-delete-char-advice)
 
 (defun xen-sp-insert-pair-advice (orig-fun &rest args)
   "Advice to disable paired delete in sp-insert-pair/sp-skip-closing-pair.  Call ORIG-FUN with ARGS."
