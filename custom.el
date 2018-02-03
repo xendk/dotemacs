1779edf7f010db6f0cf2fab55e850d54442a0869
Thomas Fini Hansen <xen@xen.dk>
2018-02-03 23:30:50 +0100

    Use hungry backward-delete-char

--- custom.el
+++ custom.el
@@ -17,6 +17,7 @@
  '(avy-keys
    (quote
     (97 98 99 100 101 102 103 104 105 106 107 108 109 110 111 112 113 114 115 116 117 118 119 120 121 122)))
+ '(backward-delete-char-untabify-method (quote all))
  '(browse-url-browser-function (quote browse-url-generic))
  '(browse-url-generic-program "sensible-browser")
  '(c-basic-offset (quote set-from-style))
