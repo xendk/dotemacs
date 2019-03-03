0214b7392574e3265ca1b0dbea5c2d83501cec21
Thomas Fini Hansen <xen@xen.dk>
2019-03-03 22:02:30 +0100

    Make sideline stand out a bit

--- custom.el
+++ custom.el
@@ -238,4 +238,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(lsp-ui-sideline-global ((t (:background "#3f444a")))))
