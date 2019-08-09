08c527ef5f96a468e8fc3f62d1a2b15f82ea6556
Thomas Fini Hansen <xen@xen.dk>
2019-08-10 00:16:19 +0200

    Set company-box background

--- custom.el
+++ custom.el
@@ -201,6 +201,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(company-box-background ((t (:background "#282c34"))) nil "Set to the light background color")
  '(cov-heavy-face ((t (:foreground "green"))))
  '(cov-light-face ((t (:foreground "orange"))))
  '(cov-none-face ((t (:foreground "red"))))
