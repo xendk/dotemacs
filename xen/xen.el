94ea2436378a0f4a02ff0bc051e9b5e8620369e9
Thomas Fini Hansen <xen@xen.dk>
2019-11-14 21:20:33 +0100

    Tighten Jira issue key regexp a tad

--- xen/xen.el
+++ xen/xen.el
@@ -313,7 +313,7 @@ See also `cycle-spacing'."
 
 (defun xen-git-commit-setup ()
   "Insert Jira issue number in commit message if branch name contain one."
-  (let ((ISSUEKEY "[[:alpha:][:digit:]]+-[[:digit:]]+"))
+  (let ((ISSUEKEY "[[:alpha:][:digit:]]\{2,\}-[[:digit:]]+"))
     (when (and (magit-get-current-branch)
                (string-match-p ISSUEKEY (magit-get-current-branch))
                (looking-at-p "\n\n#"))
