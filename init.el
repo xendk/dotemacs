c0acb94e2262cfd3dd235bd0f86aca6459e617c0
Thomas Fini Hansen <xen@xen.dk>
2021-07-05 23:23:52 +0200

    Remove obsoleted code

--- init.el
+++ init.el
@@ -64,8 +64,6 @@
   (require 'straight))
 
 ;; Bootstrap `use-package'
-;; Emacs 26 doesn't seem to know that it has nadvice.
-(customize-set-variable 'straight-built-in-pseudo-packages (quote (emacs python nadvice)))
 (straight-use-package 'use-package)
 ;; When flycheck checks this file it needs use-package (and straight
 ;; above) loaded to understand the use-package forms. It's also the
