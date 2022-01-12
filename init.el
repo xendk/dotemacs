087b9ba1d3c040bf9fd79b5ead17f7a7f3ab6532
Thomas Fini Hansen <xen@xen.dk>
2022-01-12 23:26:12 +0100

    Disable dialog boxes
    Seems that Emacs gets confused sometimes and think the command was
    mouse invoked, though it wasn't.

--- init.el
+++ init.el
@@ -617,6 +617,7 @@ candidates, unless we're in filtering mode."
   (uniquify-buffer-name-style 'post-forward-angle-brackets "Use appended brackets for file path")
   (uniquify-trailing-separator-p t "Add a slash to directory buffers")
   (url-cookie-confirmation 'nil "Don't require confirmation on cookies")
+  (use-dialog-box nil "Disable (mouse) dialogs, something is confusing emacs making it think some commands where mouse initiated")
   (user-mail-address "xen@xen.dk" "Set email address")
   (wdired-allow-to-change-permissions t "Allow C-x C-q to change permissions too")
   (whitespace-style '(face tabs tab-mark) "Make tabs more visible")
