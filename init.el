04e89e7af46367a42279e74a8cd19776d6fd7a63
Thomas Fini Hansen <xen@xen.dk>
2024-01-06 00:10:34 +0100

    Tweak vterm config

--- init.el
+++ init.el
@@ -1435,9 +1435,11 @@ candidates, unless we're in filtering mode."
   (vterm-max-scrollback 100000)
   (vterm-buffer-name-string "vterm: %s")
   (vterm-copy-mode-remove-fake-newlines t "Makes copy/paste work better")
+  (vterm-set-bold-hightbright t "Same bold color handling as most terminals")
   :bind (:map vterm-mode-map
               ;; Fish understands C-g too.
               ("C-g" . vterm--self-insert)
+              ("C-q" . vterm-send-next-key)
               ;; Rebind M/C-cursors so they'll get sent to the process.
               ("M-<up>" . vterm--self-insert)
               ("M-<down>" . vterm--self-insert)
