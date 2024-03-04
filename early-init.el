9453bb54fa8f94a4bc77747cb940c278d0069f6a
Thomas Fini Hansen <xen@xen.dk>
2024-03-04 23:03:42 +0100

    Go titlebar-less
    And move initial display options to early-init to avoid flashing.

--- early-init.el
+++ early-init.el
@@ -1,6 +1,31 @@
 ;;; early-init.el ---                                -*- lexical-binding: t; -*-
 
 (setq package-enable-at-startup nil)
+;; Let's go with without window titlebar. (At least while Gnome thinks
+;; it should make them huuuge)
+(add-to-list 'default-frame-alist '(undecorated . t))
+;; Add a bit of spacing for pop-os/shells window highlight.
+(add-to-list 'default-frame-alist '(internal-border-width . 3))
+
+;; Setup some things that can't wait for customize to load. Set
+;; foreground and background color to avoid flashing when doom-theme
+;; gets activated.
+(set-face-foreground 'default "#bbc2cf")
+(set-face-background 'default "#21242b")
+;; Set font too. TODO: This should be determined automatically:
+;; https://emacs.stackexchange.com/questions/28390/quickly-adjusting-text-to-dpi-changes
+(set-face-attribute 'default nil :height 120 :width
+                    'semi-condensed :foundry "FBI " :family "Input")
+;; Disable the mode-line in the "initial buffer" which is shown while
+;; initializing. This prevents ugly old-school mode-line showing up
+;; before doom-modeline has done its thing.
+(setq mode-line-format nil)
+;; Disable tool-bar.
+(tool-bar-mode 0)
+;; Disable scroll-bars.
+(scroll-bar-mode 0)
+;; Disable menu-bar.
+(menu-bar-mode 0)
 
 ;; Local Variables:
 ;; no-byte-compile: t
--- init.el
+++ init.el
@@ -4,25 +4,6 @@
 ;; My Emacs init.el.
 
 ;;; Code:
-;; Setup some things that can't wait for customize to load. Set
-;; foreground and background color to avoid flashing when doom-theme
-;; gets activated.
-(set-face-foreground 'default "#bbc2cf")
-(set-face-background 'default "#21242b")
-;; Set font too. TODO: This should be determined automatically:
-;; https://emacs.stackexchange.com/questions/28390/quickly-adjusting-text-to-dpi-changes
-(set-face-attribute 'default nil :height 120 :width
-                    'semi-condensed :foundry "FBI " :family "Input")
-;; Disable the mode-line in the "initial buffer" which is shown while
-;; initializing. This prevents ugly old-school mode-line showing up
-;; before doom-modeline has done its thing.
-(setq mode-line-format nil)
-;; Disable tool-bar.
-(tool-bar-mode 0)
-;; Disable scroll-bars.
-(scroll-bar-mode 0)
-;; Disable menu-bar.
-(menu-bar-mode 0)
 
 ;; Start server if not already running.
 (require 'server)
