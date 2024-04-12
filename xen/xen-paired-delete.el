b442247bd144611a712b624c6a35e21d01fc7bde
Thomas Fini Hansen <xen@xen.dk>
2024-04-12 21:24:01 +0200

    Add ignored modes to xen-paired-delete-mode

--- xen/xen-paired-delete.el
+++ xen/xen-paired-delete.el
@@ -29,6 +29,14 @@
 
 (defvar xen-paired-delete-char-disabled)
 
+(defcustom xen-paired-delete-ignore-modes-list '(
+                                                 minibuffer-inactive-mode
+                                                 vterm-mode
+                                                 )
+  "Modes where xen-paired-delete mode is inactive if allowed globally."
+  :type '(repeat symbol)
+  :group 'xen-paired-delete)
+
 (defun xen-paired-delete (backwards-p &optional arg)
   "Deletes the matching pair if deleting a pair.
 
@@ -53,17 +61,18 @@ only work when ARG is 1 or the region is not active."
            (t nil)))))))
 
 (defun xen-paired-delete-delete-char-advice (orig-fun n &optional kill-flag)
-  "Advice for delete char.  ORIG-FUN is the overriden function. Pass N and KILL-FLAG to original."
-  (if (not (boundp 'xen-paired-delete-char-disabled))
+  "Advice for delete char.  ORIG-FUN is the overridden function. Pass N and KILL-FLAG to original."
+  (if (and xen-paired-delete-mode
+           (not
+            (boundp 'xen-paired-delete-char-disabled)))
       (let ((xen-paired-delete-char-disabled t))
         (save-match-data (progn
                            (if (not (xen-paired-delete (> 0 n) (abs n)))
                                (funcall orig-fun n kill-flag)))))
     (funcall orig-fun n kill-flag)))
-;; (advice-add 'delete-char :around #'xen-paired-delete-delete-char-advice)
 
-(defun xen-paired-delete-sp-insert-pair-advice (orig-fun &rest args)
-  "Advice to disable paired delete in sp-insert-pair/sp-skip-closing-pair.  Call ORIG-FUN with ARGS."
+(defun xen-paired-delete-disable-advice (orig-fun &rest args)
+  "Advice to disable paired delete in for some functions.  Call ORIG-FUN with ARGS."
   (let ((xen-paired-delete-char-disabled t))
     (apply orig-fun args)))
 
@@ -75,15 +84,35 @@ only work when ARG is 1 or the region is not active."
   (if xen-paired-delete-mode
       (progn
         (advice-add 'delete-char :around #'xen-paired-delete-delete-char-advice)
-        (advice-add 'sp-insert-pair :around #'xen-paired-delete-sp-insert-pair-advice)
-        (advice-add 'sp-skip-closing-pair :around #'xen-paired-delete-sp-insert-pair-advice))
+        (advice-add 'sp-insert-pair :around #'xen-paired-delete-disable-advice)
+        (advice-add 'sp-skip-closing-pair :around #'xen-paired-delete-disable-advice))
     (advice-remove 'delete-char #'xen-paired-delete-delete-char-advice)
-    (advice-remove 'sp-insert-pair #'xen-paired-delete-sp-insert-pair-advice)
-    (advice-remove 'sp-skip-closing-pair #'xen-paired-delete-sp-insert-pair-advice)))
+    (advice-remove 'sp-insert-pair #'xen-paired-delete-disable-advice)
+    (advice-remove 'sp-skip-closing-pair #'xen-paired-delete-disable-advice))
+  )
+
+(defun turn-on-xen-paired-delete-mode ()
+  "Turn on `xen-paired-delete-mode'.
+
+This function is used to turn on `global-xen-paired-delete-mode'.
+
+By default `global-xen-paired-delete-mode' ignores buffers with
+`mode-class' set to special, but only if they are also not comint
+buffers.
+
+Additionally, buffers on `xen-paired-delete-ignore-modes-list' are ignored.
+
+You can still turn on xen-paired-delete-mode in these mode manually (or
+in mode's startup-hook etc.) by calling `xen-paired-delete-mode'."
+  (interactive)
+  (unless (or (member major-mode xen-paired-delete-ignore-modes-list)
+              (and (not (derived-mode-p 'comint-mode))
+                   (eq (get major-mode 'mode-class) 'special)))
+    (xen-paired-delete-mode t)))
 
 (define-globalized-minor-mode global-xen-paired-delete-mode
   xen-paired-delete-mode
-  xen-paired-delete-mode)
+  turn-on-xen-paired-delete-mode)
 
 (provide 'xen-paired-delete)
 ;;; xen-paired-delete.el ends here
