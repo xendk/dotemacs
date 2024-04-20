4c696be2aabb0d5a12a6824e98f36897cc172a52
Thomas Fini Hansen <xen@xen.dk>
2024-04-20 21:18:41 +0200

    Setupify rainbow-mode

--- init.el
+++ init.el
@@ -411,6 +411,13 @@
      "M-<up>" region-occurrences-highlighter-prev
      "M-<down>" region-occurrences-highlighter-next)))
 
+(setup rainbow-mode
+  (:elpaca t)
+  (:also-load +rainbow-mode)
+  (advice-add #'rainbow-colorize-match :override #'+rainbow-colorize-match)
+  (advice-add #'rainbow-turn-off :after #'+rainbow-clear-overlays)
+  (:hook-into css-mode))
+
 ;;; Packages.
 
 ;; Reinstall these when the need arise:
@@ -1191,27 +1198,6 @@ targets."
   :bind (:map project-prefix-map
               ("m" . magit-project-status))
   :elpaca nil)
-
-(use-package rainbow-mode
-  :defer t
-  :config
-  ;; Use overlay instead of text properties to override `hl-line' faces.
-  ;; Taken from https://github.com/seagle0128/.emacs.d/blob/bf3d66248b883638d0b7ab584d5074f019fb555d/lisp/init-highlight.el#L148
-  (defun xen-rainbow-colorize-match (color &optional match)
-    (let* ((match (or match 0))
-           (ov (make-overlay (match-beginning match) (match-end match))))
-      (overlay-put ov 'ovrainbow t)
-      (overlay-put ov 'face `((:foreground ,(if (> 0.5 (rainbow-x-color-luminance color))
-                                                "white" "black"))
-                              (:background ,color)))))
-  (advice-add #'rainbow-colorize-match :override #'xen-rainbow-colorize-match)
-
-  (defun xen-rainbow-clear-overlays ()
-    "Clear all rainbow overlays."
-    (remove-overlays (point-min) (point-max) 'ovrainbow t))
-  (advice-add #'rainbow-turn-off :after #'xen-rainbow-clear-overlays)
-  :hook
-  (css-mode . rainbow-mode))
 
 (use-package reaper
   :config
---
+++ lisp/+rainbow-mode.el
@@ -0,0 +1,24 @@
+;;; +rainbow-mode.el --- rainbow-mode additions      -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;; Use overlay instead of text properties to override `hl-line' faces.
+;; Taken from https://github.com/seagle0128/.emacs.d/blob/bf3d66248b883638d0b7ab584d5074f019fb555d/lisp/init-highlight.el#L148
+
+;;; Code:
+
+(defun +rainbow-colorize-match (color &optional match)
+  "Override `rainbow-colorize-match' to use overlays."
+  (let* ((match (or match 0))
+         (ov (make-overlay (match-beginning match) (match-end match))))
+    (overlay-put ov 'ovrainbow t)
+    (overlay-put ov 'face `((:foreground ,(if (> 0.5 (rainbow-x-color-luminance color))
+                                              "white" "black"))
+                            (:background ,color)))))
+
+(defun +rainbow-clear-overlays ()
+  "Clear all rainbow overlays."
+  (remove-overlays (point-min) (point-max) 'ovrainbow t))
+
+(provide '+rainbow-mode)
+;;; +rainbow-mode.el ends here
