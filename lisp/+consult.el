771c18d4b195f4f7e745341e26c66ad187eb723d
Thomas Fini Hansen <xen@xen.dk>
2025-04-25 22:17:37 +0200

    Fix up consult--multi-group-by-project so it doesn't trigger errors
    And general cleanup.

--- lisp/+consult.el
+++ lisp/+consult.el
@@ -4,6 +4,13 @@
 
 ;;; Code:
 
+(declare-function consult--multi "consult")
+(declare-function consult-line "consult")
+(declare-function consult-ripgrep "consult")
+(declare-function consult--buffer-action "consult")
+(declare-function project-root (PROJECT) "project")
+(defvar consult-buffer-sources)
+
 (defun +consult-line ()
   "Call consult-line, using region as initial input, if active."
   (interactive)
@@ -24,7 +31,9 @@
     (consult-ripgrep)))
 
 (defun +consult-buffer-by-project (&optional sources)
-  "Variant of `consult-buffer' that groups by project."
+  "Variant of `consult-buffer' that groups by project.
+
+SOURCES is passed to consult--multi."
   (interactive)
   (let ((selected (consult--multi (or sources consult-buffer-sources)
                                   :require-match
@@ -39,13 +48,23 @@
       (consult--buffer-action (car selected)))))
 
 (defun +consult--multi-group-by-project (cand transform)
+  "Group candidates by project.
+
+Return project for CAND or TRANSFORM the candidate."
   (if transform
       cand
-    (let* ((default-directory (buffer-local-value 'default-directory (get-buffer (cdr (get-text-property 0 'multi-category cand)))))
-           (project (project-current nil)))
-      (if project
-          (project-root project)
-        "<no project>"))))
+    ;; When using p <spc> to narrow buffers to current project,
+    ;; there's no buffer for the candidates for other projects that
+    ;; `consult-buffer' shows. So in this case we return the default
+    ;; "Project Root" string and hope it fits.
+    (let ((buffer (get-buffer (cdr (get-text-property 0 'multi-category cand)))))
+      (if buffer
+          (let* ((default-directory (buffer-local-value 'default-directory buffer))
+                 (project (project-current nil)))
+            (if project
+                (project-root project)
+              "<no project>"))
+        "Project Root"))))
 
 (provide '+consult)
 ;;; +consult.el ends here
