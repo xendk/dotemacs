2ad4e3507882e31bba8176fe8bacb98eb43d2975
Thomas Fini Hansen <xen@xen.dk>
2025-07-12 22:36:49 +0200

    Update custom-safe-themes again
    I should find a better way.

--- custom.el
+++ custom.el
@@ -4,7 +4,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("fd22a3aac273624858a4184079b7134fb4e97104d1627cb2b488821be765ff17"
+   '("4e6ee061bdca070a73c1bf0d185d294231b4b9d694088bd2046250b7677d073f"
+     "fd22a3aac273624858a4184079b7134fb4e97104d1627cb2b488821be765ff17"
      "87fa3605a6501f9b90d337ed4d832213155e3a2e36a512984f83e847102a42f4"
      "dfcd2b13f10da4e5e26eb1281611e43a134d4400b06661445e7cbb183c47d2ec"
      "571661a9d205cb32dfed5566019ad54f5bb3415d2d88f7ea1d00c7c794e70a36"
