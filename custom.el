0390ac27b82668e0e29c4bd42498128223b9aa0b
Thomas Fini Hansen <xen@xen.dk>
2025-07-12 22:13:50 +0200

    Add some custom stuff that's shown up

--- custom.el
+++ custom.el
@@ -3,4 +3,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(custom-safe-themes
+   '("fd22a3aac273624858a4184079b7134fb4e97104d1627cb2b488821be765ff17"
+     "87fa3605a6501f9b90d337ed4d832213155e3a2e36a512984f83e847102a42f4"
+     "dfcd2b13f10da4e5e26eb1281611e43a134d4400b06661445e7cbb183c47d2ec"
+     "571661a9d205cb32dfed5566019ad54f5bb3415d2d88f7ea1d00c7c794e70a36"
+     "7c28419e963b04bf7ad14f3d8f6655c078de75e4944843ef9522dbecfcd8717d"
+     "34cf3305b35e3a8132a0b1bdf2c67623bc2cb05b125f8d7d26bd51fd16d547ec"
+     default))
+ '(ignored-local-variable-values '((xen-php-bin-dir . "sites/all/vendor/bin")))
+ '(package-selected-packages '(cask)))
