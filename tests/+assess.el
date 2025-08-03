f0b68241c607609f68592a0b22a2257c72e8f4ab
Thomas Fini Hansen <xen@xen.dk>
2025-08-03 11:37:41 +0200

    Fix assess indentation

--- tests/+assess.el
+++ tests/+assess.el
@@ -10,12 +10,12 @@
     (it "does nothing if no (require 'assess)"
       (spy-on 'require)
       (assess-as-temp-buffer ";;; Just a lispy file"
-                             (+assess-maybe-require)
-                             (expect 'require :not :to-have-been-called)))
+        (+assess-maybe-require)
+        (expect 'require :not :to-have-been-called)))
 
     (it "requires 'assess if (require 'assess) found"
       (spy-on 'require)
       (assess-as-temp-buffer ";;; Just a lispy file
 (require 'assess)"
-                             (+assess-maybe-require)
-                             (expect 'require :to-have-been-called-with 'assess)))))
+        (+assess-maybe-require)
+        (expect 'require :to-have-been-called-with 'assess)))))
--- tests/+buttercup.el
+++ tests/+buttercup.el
@@ -9,11 +9,11 @@
   (describe "+buttercup-minor-mode-maybe"
     (it "does nothing if no (require 'buttercup)"
       (assess-as-temp-buffer ";;; Just a lispy file"
-                             (+buttercup-minor-mode-maybe)
-                             (expect buttercup-minor-mode :not :to-be-truthy)))
+        (+buttercup-minor-mode-maybe)
+        (expect buttercup-minor-mode :not :to-be-truthy)))
 
     (it "enables buttercup-minor-mode if (require 'buttercup) found"
       (assess-as-temp-buffer ";;; Just a lispy file
 (require 'buttercup)"
-                             (+buttercup-minor-mode-maybe)
-                             (expect buttercup-minor-mode :to-be-truthy)))))
+        (+buttercup-minor-mode-maybe)
+        (expect buttercup-minor-mode :to-be-truthy)))))
