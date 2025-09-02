5917ef6b6549c2f2bd0d3e7fca3fc7437caee0ce
Thomas Fini Hansen <xen@xen.dk>
2025-09-02 23:13:31 +0200

    Rename indentinator

--- init.el
+++ init.el
@@ -1145,7 +1145,7 @@ set (i.e., OPERATION is \\='set).  This excludes, e.g., let bindings."
        "!" 'consult-flycheck))))
 
 (setup indentinator
-  (:elpaca :host github :repo "xendk/indentinator")
+  (:elpaca :host github :repo "xendk/indentinator.el")
   (:option
    ;; Speed up reindentation
    indentinator-idle-time 0.005)
