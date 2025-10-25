e625cb35fe90e1d73f2d176589f53f62fa1334cc
Thomas Fini Hansen <xen@xen.dk>
2025-10-25 22:09:12 +0200

    Move old xen php-make-use test to root tests

--- tests/+php-mode.el
+++ tests/+php-mode.el
@@ -1,19 +1,20 @@
-;; -*- lexical-binding: t -*-
+;;; -*- lexical-binding: t; -*-
 
-(require 'php-mode)
-(require 'tests/xen-test-helpers)
-(require 'xen-php)
+(require 'buttercup)
 
-(describe "xen-make-use"
+(require 'tests/+helpers)
+(require '+php-mode)
+
+(describe "+php-make-use"
   (it "should move class to use block"
-    (xen-test-with-temp-php-buffer
+    (+test-with-temp-php-buffer
      "
 use Something
 
 func(\\Namespaced\\Cl|ass);
 "
-     (xen-php-make-use)
-     (xen-expect-buffer-equals "
+     (+php-make-use)
+     (+expect-buffer-equals "
 use Namespaced\\Class;
 use Something
 
@@ -21,55 +22,55 @@ func(Class);
 ")))
 
   (it "should do nothing if not on a valid FQ-name"
-    (xen-test-with-temp-php-buffer
+    (+test-with-temp-php-buffer
      "
 use Something
 
 func(\\Cl|ass);
 "
-     (xen-php-make-use)
-     (xen-expect-buffer-equals "
+     (+php-make-use)
+     (+expect-buffer-equals "
 use Something
 
 func(\\Class);
 ")))
 
   (it "should not duplicate uses"
-    (xen-test-with-temp-php-buffer
+    (+test-with-temp-php-buffer
      "
 use Namespaced\\Class;
 use Something
 
 func(\\Namespaced\\Cl|ass);
 "
-     (xen-php-make-use)
-     (xen-expect-buffer-equals "
+     (+php-make-use)
+     (+expect-buffer-equals "
 use Namespaced\\Class;
 use Something
 
 func(Class);
 ")))
   (it "should add in a use block if none exists"
-      (xen-test-with-temp-php-buffer
-       "
+    (+test-with-temp-php-buffer
+     "
 func(\\Namespaced\\Cl|ass);
 "
-       (xen-php-make-use)
-       (xen-expect-buffer-equals "
+     (+php-make-use)
+     (+expect-buffer-equals "
 use Namespaced\\Class;
 
 func(Class);
 ")))
   (it "should add in a use block after namespace if none exists"
-      (xen-test-with-temp-php-buffer
-       "<php
+    (+test-with-temp-php-buffer
+     "<?php
 
 namespace Banana;
 
 func(\\Namespaced\\Cl|ass);
 "
-       (xen-php-make-use)
-       (xen-expect-buffer-equals "<php
+     (+php-make-use)
+     (+expect-buffer-equals "<?php
 
 namespace Banana;
 
