e6472174180293d0c553c6b7662e950e553c2e5b
Thomas Fini Hansen <xen@xen.dk>
2025-10-26 16:39:33 +0100

    Fix style of +highlight-symbol.el

--- lisp/+highlight-symbol.el
+++ lisp/+highlight-symbol.el
@@ -4,14 +4,19 @@
 
 ;;; Code:
 
+(declare-function highlight-symbol-mode "highlight-symbol")
+(defvar highlight-symbol-mode)
+
 (defun +highlight-symbol-mode-deactivate ()
   "Temporarily disable highlight-symbol-mode."
-  (when highlight-symbol-mode
+  (when (bound-and-true-p highlight-symbol-mode)
     (setq-local highlight-symbol-mode-suspend t)
     (highlight-symbol-mode 0)))
 
 (defun +highlight-symbol-mode-reactivate ()
-  "Reactive highlight-symbol-mode after `+highlight-symbol-mode-deactivate' deactivated it."
+  "Reactive highlight-symbol-mode after we deactivated it.
+
+Undo the work of `+highlight-symbol-mode-deactivate'."
   (when (bound-and-true-p highlight-symbol-mode-suspend)
     (kill-local-variable highlight-symbol-mode-suspend)
     (highlight-symbol-mode 1)))
